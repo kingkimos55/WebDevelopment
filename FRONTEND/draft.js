172e5f452878877fe9a3700fcecd43729ea09b00
kingkimos55 <mosesubongekpe@gmail.com>
2024-03-18 12:31:10 +0100

    Second Commit

---
+++ FRONTEND/draft.js
@@ -0,0 +1,15 @@
+const board = document.getElementById("board");
+
+for (let i = 0; i = 0< 8; i++){
+    for (let j = 0; j < 8; j++){
+        if (i % 2 >= 1){
+            board.innerHTML +=
+            
+        }
+
+        board.innerHTML+=`
+        
+        `;
+        
+    }
+}
